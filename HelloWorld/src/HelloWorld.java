82347026bee0f731590409d30f9a7f7beae9146c
Amr Abdelraouf <amr.abdelraouf93@gmail.com>
2015-10-25 22:59:13 +0100

    adds HelloUser file, modifies HelloWorld

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  */
@@ -10,9 +14,15 @@ public class HelloWorld {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello ASE2015 - how are you? :)");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please enter your name: ");
+		String userName = br.readLine();
+		HelloUser user = new HelloUser(userName);
+		user.greetUser();
 	}
 
 }
